9b26ceb837187e5d90f08c66a980a7de4f189511
huuthong11221 <nguyenhuuthong792004@gmail.com>
2025-04-07 00:27:07 +0700

    Chức năng order(User,Admin)

---
+++ Webshopping/Webshopping/Areas/Admin/Controllers/OrderController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Webshopping.Repository;
+
+namespace Webshopping.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Route("admin/")]
+    public class OrderController : Controller
+    {
+        private readonly DataContext _dataContext;
+        public OrderController(DataContext context)
+        {
+            _dataContext = context;
+        }
+        [HttpGet("Order")]
+        public async Task<IActionResult> Index()
+        {
+            return View(await _dataContext.Orders.OrderByDescending(p => p.Id).ToListAsync());
+        }
+        public async Task<IActionResult> ViewOrder(string ordercode)
+        {
+            var DetailsOrder = await _dataContext.OrderDetails.Include(od => od.Product).Where(od => od.OrderCode == ordercode).ToListAsync();
+            return View(DetailsOrder);
+        }
+    }
+}
