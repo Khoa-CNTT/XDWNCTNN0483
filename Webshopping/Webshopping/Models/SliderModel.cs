04782f07f8e0829aecc3c102aab6a006f816cd25
Kim trong <trong17114006@gmail.com>
2025-04-10 10:15:01 +0700

    Slider

---
+++ Webshopping/Webshopping/Models/SliderModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+namespace Webshopping.Models
+{
+	public class SliderModel
+	{
+		public int Id { get; set; }
+		[Required(ErrorMessage = "Yêu cầu nhập tên slider")]
+		public string Name { get; set; }
+		[Required(ErrorMessage = "Yêu cầu nhập mô tả của slider")]
+		public string Description { get; set; }
+		
+		public int? Status { get; set; }
+		public string Img { get; set; }
+		
+		[NotMapped]
+		[FileExtensions]
+		public IFormFile ImageUpload { get; set; }
+	}
+}
