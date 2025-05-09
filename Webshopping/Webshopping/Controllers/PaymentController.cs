daf0563e23247b242a7ec0276ae135e048985d7d
Kim trong <trong17114006@gmail.com>
2025-05-09 15:41:57 +0700

    Momo_Zalopay

---
+++ Webshopping/Webshopping/Controllers/PaymentController.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Webshopping.Models;
+using Webshopping.Services.Momo;
+using Webshopping.Services.ZaloPay;
+
+namespace Webshopping.Controllers
+{
+	
+	public class PaymentController : Controller
+	{
+		private IMomoService _momoService;
+
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+		[HttpPost]
+		public async Task<IActionResult> CreatePaymentMomo(MomoInfoModel model)
+		{
+			var response = await _momoService.CreatePaymentAsync(model);
+
+			return Redirect(response.PayUrl);
+
+		}
+
+		[HttpGet]
+		public IActionResult PaymentCallback()
+		{
+			var response = _momoService.PaymentExecuteAsync(HttpContext.Request.Query);
+			return View(response);
+		}
+		private readonly IZaloPayService _zaloPayService;
+
+		public PaymentController(IMomoService momoService, IZaloPayService zaloPayService)
+		{
+			_momoService = momoService;
+			_zaloPayService = zaloPayService;
+		}
+
+		[HttpPost]
+		[Route("CreateZaloPayPaymentUrl")]
+		public async Task<IActionResult> CreateZaloPayPaymentUrl(ZaloInfoModel model)
+		{
+			var paymentUrl = await _zaloPayService.CreatePaymentUrlAsync(model);
+			return Redirect(paymentUrl);
+		}
+		[HttpGet]
+		public IActionResult PaymentCallBack()
+		{
+			
+			var query = Request.Query;
+			var apptransid = query["apptransid"];
+			var status = query["status"];
+
+			if (status == "1")
+			{
+				ViewBag.Message = "Thanh toán thành công qua ZaloPay!";
+			}
+			else
+			{
+				ViewBag.Message = "Thanh toán thất bại hoặc bị huỷ!";
+			}
+
+			return View(); 
+		}
+
+
+	}
+}
---
+++ Webshopping/Webshopping/Services/Momo/IMomoService.cs
@@ -0,0 +1,13 @@
+﻿using Webshopping.Models;
+using Webshopping.Models.Momo;
+using Microsoft.AspNetCore.Http;
+using System.Threading.Tasks;
+
+namespace Webshopping.Services.Momo
+{
+	public interface IMomoService
+	{
+		Task<MomoCreatePaymentResponseModel> CreatePaymentAsync(MomoInfoModel model);
+		MomoExecuteResponseModel PaymentExecuteAsync(IQueryCollection collection);
+	}
+}
